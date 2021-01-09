2c16d05c65ad69d027c92ec91e099e3931e1c7a8
2L <loiacono.leandro@gmail.com>
2021-01-09 04:05:49 +0100

    development

--- main.py
+++ main.py
@@ -20,54 +20,109 @@ class RacZ(GridLayout):
 
 class RacApp(App):
 
+  START_X = 0
+  START_Y = 200
+  START_Z = 120
+  START_E = 0
   isRelative = BooleanProperty(False)
-  step = NumericProperty(1)
+  step = NumericProperty(10)
   console = StringProperty("")
   consoleBufferSize = 100
   consoleSize = 0
+  x = NumericProperty(0)
+  y = NumericProperty(200)
+  z = NumericProperty(120)
+  e = NumericProperty(0)
+  f = NumericProperty(1)
+  customCmd = StringProperty("")
+  motorsToggleState = StringProperty("normal")
+  port = StringProperty("")
+  ports_available = StringProperty("")
+
 
   sender = None
 
   def build(self):
     self.root = Rac()
     self.sender = SerialSender(self)
+    self.port = self.sender.port
 
   def appendOnConsole(self, message):
       if (self.consoleSize < self.consoleBufferSize):
-          self.console += message + '\n'
+          self.console += str(message) + '\n'
           self.consoleSize += 1
       else:
-          self.console = message + '\n'
+          self.console = str(message) + '\n'
           self.consoleSize = 0
 
-  def move_relative(self, x, y, z):
+  def move_relative(self, x = 0.0, y = 0.0, z = 0.0, e = 0.0):
     if(self.isRelative == False):
       self.sender.send_command('G91')
       self.isRelative = True
 
-    cmd = 'G1 X' + str(x*self.step) + ' Y' + str(y*self.step) + ' Z' + str(z*self.step)
-    self.sender.send_command(cmd)
+    self.x = x
+    self.y = y
+    self.z = z
+    self.e = e
+    cmd = 'G1 X' + str(x*self.step) + ' Y' + str(y*self.step) + ' Z' + str(z*self.step) + ' E' + str(e*self.step)
+    self.send_command(cmd)
 
-  def home(self, x, y, z):
+  def home(self, x = 0, y = 0, z = 0, E = 0):
     if(self.isRelative == True):
-      self.sender.send_command('G90')
+      self.send_command('G90')
       self.isRelative = False
     if( x==1 and y ==1 and z==1):
       cmd = 'G28'
     else:
       cmd = 'G1'
       if(x == 1):
-        cmd += ' X0'
+        cmd += ' X' + str(self.START_X)
       if(y == 1):
-        cmd += ' Y200'
+        cmd += ' Y' + str(self.START_Y)
       if(z == 1):
-        cmd += ' Z120'
-    self.sender.send_command(cmd)
+        cmd += ' Z' + str(self.START_Z)
+      if(z == 1):
+        cmd += ' E' + str(self.START_E)
+    ret = self.send_command(cmd)
+    self.updateGUI(ret)
 
-  def toggleMotors(self, toggleMottors):
-    if(toggleMottors.state == 'down'):
+  def toggleMotors(self, toggleMotors):
+    if(toggleMotors.state == 'down'):
         self.sender.send_command('M17')
     else:
         self.sender.send_command('M18')
+  
+  def toggleGripper(self, toggleGripper):
+    if(toggleGripper.state == 'down'):
+        self.sender.send_command('M3')
+    else:
+        self.sender.send_command('M5')
+  
+  def setStep(self, step):
+    self.step = float(step)
+  
+  def setPort(self, step):
+    self.sender.port = step
+    self.sender.connect()
+
+  def send_command(self, cmd):
+    msg = self.sender.send_command(cmd)
+    if(msg != None):
+      self.appendOnConsole(msg)
+
+  def send_gcode(self, gcode):
+    cmd = gcode.text
+    ret = self.send_command(cmd)
+    if(ret != 'Not connected'):
+      gcode.text = ''
+      self.updateGUI(ret)
+      
+  
+  def updateGUI(self, cmd):
+    print(cmd)
+    if(cmd == 'M17' or cmd == 'G28' or cmd == 'm17' or cmd == 'g28'):
+      self.motorsToggleState = 'down'
+    if(cmd == 'M18' or cmd == 'm18'):
+      self.motorsToggleState = 'normal'
 
 RacApp().run()
--- serialsender.py
+++ serialsender.py
@@ -15,19 +15,30 @@ waterX = -220
 waterY = 150
 
 class SerialSender():
-    port = '/dev/ttyUSB0'
+    port = ''
     baud = 115200
     timeout = None
     ser = None
     connected = False
     parent = None
+    ports_available = ""
 
     def __init__(self, parent):
         super(SerialSender, self).__init__()
         self.parent = parent
+        self.serial_ports()
+        #self.connect()
+
+    def connect(self):
         try:
+            msg = "Trying to connect to port " + self.port
+            print(msg)
+            self.parent.appendOnConsole(msg)
             self.ser = serial.Serial(self.port,self.baud,timeout=self.timeout)
             self.connected = True
+            msg = "Connected!"
+            print(msg)
+            self.parent.appendOnConsole(msg)
         except (OSError, serial.SerialException):
             msg = "Serial port not available, please chose a different one"
             print(msg)
@@ -42,13 +53,21 @@ class SerialSender():
         print(msg)
         self.parent.appendOnConsole(msg)
         all_ports = set()
+        i = 0
         for ap, _, _ in all_port_tuples:
             p = os.path.basename(ap)
             print(p)
             if p.startswith("ttyUSB") or p.startswith("ttyACM"):
                 all_ports |= {ap}
                 #logging.info("\t%s", str(ap))
-                msg = "\t" + str(ap)
+                msg = str(ap)
+                if(self.ports_available != ""):
+                    self.ports_available += ","
+                self.parent.ports_available = msg
+                if(i == 0):
+                    self.port = msg
+                i = i + 1
+                msg = "\t" + msg
                 print(msg)
                 self.parent.appendOnConsole(msg)
 
@@ -74,17 +93,21 @@ class SerialSender():
             a = self.ser.readline()
             if "ok" in a.decode("utf-8"):
                 waitstatus = 0
-                break
+                return None
             else:
-                print(a)
+                return a
 
     def send_command(self, cmd_param):
         if(self.connected):
             bCmd = (cmd_param + '\r').encode('utf-8')
             self.ser.write(bCmd)
-            self.parent.appendOnConsole(cmd_param)
-            print(bCmd)
-            self.wait_complete()
+            #print(bCmd)
+            ret = self.wait_complete()
             time.sleep(0.1)
+            return ret
         else:
-            self.parent.appendOnConsole("Not connected")
+            return "Not connected"
+    
+    #def set_port(self, port):
+    #    self.port = port
+    #    self.connect()
